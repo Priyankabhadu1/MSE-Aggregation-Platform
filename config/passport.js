79b564e6088a80d4e06183b8c235f7c9d2408b1d
shikhar09 <shikhardixit09@gmail.com>
2026-02-23 13:01:35 +0530

    implement LocalStrategy using email as username field

--- config/passport.js
+++ config/passport.js
@@ -17,3 +17,27 @@ passport.deserializeUser(async (id, done) => {
     done(err, null);
   }
 });
+
+// Local Search Strategy
+passport.use(new LocalStrategy(
+  { usernameField: 'email' },
+  async (email, password, done) => {
+    try {
+      const user = await User.findOne({ email: email.toLowerCase() });
+      if (!user) {
+        return done(null, false, { message: 'No account with that email.' });
+      }
+      if (!user.password) {
+        return done(null, false, { message: 'This account uses Google login. Please sign in with Google.' });
+      }
+      const isMatch = await user.comparePassword(password);
+      if (!isMatch) {
+        return done(null, false, { message: 'Incorrect password.' });
+      }
+      return done(null, user);
+    } catch (err) {
+      return done(err);
+    }
+  }
+));
+
