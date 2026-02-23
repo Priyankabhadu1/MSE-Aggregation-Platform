60bf79db6faf6742d1207245d727524face69ba6
shikhar09 <shikhardixit09@gmail.com>
2026-02-23 12:59:53 +0530

    initialize passport configuration with serialize/deserialize logic

---
+++ config/passport.js
@@ -0,0 +1,19 @@
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const User = require('../models/User');
+
+// Serialize user into session
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+// Deserialize user from session
+passport.deserializeUser(async (id, done) => {
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (err) {
+    done(err, null);
+  }
+});
