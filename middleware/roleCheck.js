b195ed046490bf2f84f8693dc0a3b022bcd798ff
cinnamonshake45 <shikhardixit09@gmail.com>
2026-04-17 09:30:00 +0530

    added rolecheck middleware

---
+++ middleware/roleCheck.js
@@ -0,0 +1,21 @@
+const roleCheck = (...roles) => {
+  return (req, res, next) => {
+    if (!req.session.user) return res.redirect('/login');
+    if (!roles.includes(req.session.user.role)) {
+      return res.status(403).send('Access denied');
+    }
+    next();
+  };
+};
+
+const mseApproved = async (req, res, next) => {
+  if (req.session.user.role !== 'mse') return next();
+  const User = require('../models/User');
+  const user = await User.findById(req.session.user.id);
+  if (!user.isApproved) {
+    return res.status(403).send('Your account is pending admin approval. Please wait.');
+  }
+  next();
+};
+
+module.exports = { roleCheck, mseApproved };
